b80f276a73cb9564bb49c2f3955a67a3b412ac76
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-27 18:54:36 +0100

    db started

--- cadUsuario/models.py
+++ cadUsuario/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    id_users = models.AutoField(primary_key=True)
+    names = models.TextField(max_length=255)
+    ages = models.IntegerField()
